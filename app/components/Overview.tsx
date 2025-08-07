453e32358c099d5de627ee87b15a0d79eefaa9ca
Whoiamx <gaastontimchuk@gmail.com>
2025-08-07 00:18:20 -0300

    better page

--- app/components/Overview.tsx
+++ app/components/Overview.tsx
@@ -1,7 +1,8 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
-import { ChevronRight, DollarSign } from "lucide-react";
+import { ChevronRight } from "lucide-react";
+import { Categories } from "./Categories";
 
 export function Overview() {
   return (
@@ -58,37 +59,12 @@ export function Overview() {
             </CardHeader>
             <CardContent className="space-y-4">
               <div className="flex items-center space-x-4">
-                <div className="w-12 h-12 bg-teal-100 rounded-lg flex items-center justify-center">
-                  <DollarSign className="h-6 w-6 text-teal-600" />
-                </div>
                 <div>
                   <p className="text-sm text-gray-600">Total Ahorrado</p>
                   <p className="text-2xl font-bold">$850</p>
                 </div>
               </div>
-
-              <div className="grid grid-cols-2 gap-4">
-                <div className="space-y-2">
-                  <div className="flex justify-between">
-                    <span className="text-sm text-gray-600">Ahorros</span>
-                    <span className="text-sm font-medium">$159</span>
-                  </div>
-                  <div className="flex justify-between">
-                    <span className="text-sm text-gray-600">Concierto</span>
-                    <span className="text-sm font-medium">$110</span>
-                  </div>
-                </div>
-                <div className="space-y-2">
-                  <div className="flex justify-between">
-                    <span className="text-sm text-gray-600">Regalo</span>
-                    <span className="text-sm font-medium">$40</span>
-                  </div>
-                  <div className="flex justify-between">
-                    <span className="text-sm text-gray-600">Laptop Nueva</span>
-                    <span className="text-sm font-medium">$10</span>
-                  </div>
-                </div>
-              </div>
+              <Categories />
             </CardContent>
           </Card>
 
---
+++ app/wallet/ahorros/page.tsx
@@ -0,0 +1,7 @@
+export default function AhorrosPage() {
+  return (
+    <div>
+      <h1>Hello AhorrosPage</h1>
+    </div>
+  );
+}
